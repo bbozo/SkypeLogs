5be9fcca5370829ad7fffac67022e1f348777e8d
Borna Novak <dosadnizub@gmail.com>
2014-01-09 09:24:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkypeLogs::Application.config.secret_key_base = 'ae5a03758c78e925d24102357fd3cf7143e475adc883cec408634260180132903817433caadf612e30ada7d2a0db13d75b36f4d113e9f6a3b32b4b5bf6cda30a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SkypeLogs::Application.config.session_store :cookie_store, key: '_SkypeLogs_session'
