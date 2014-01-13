08dca7101294ab7c11d84793f0ec7eecb170e42d
Borna Novak <dosadnizub@gmail.com>
2014-01-13 12:51:40 +0100

    Added relations to messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,7 @@
 class Conversation < AbstractSkypeModel
   skype_table_name!
+
+  has_many :participants, foreign_key: :convo_id
+  has_many :messages, foreign_key: :convo_id
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,15 +2,18 @@ class Message < AbstractSkypeModel
   skype_table_name!
 
   belongs_to :conversation, foreign_key: :convo_id
+  has_many :participants, through: :conversation
 
   skype_time_columns :timestamp
+
+  scope :with_participant, lambda{ |login| joins(:participants).where{participants.identity =~ "%#{login}%"} }
 
   def body
     @body ||= Nokogiri::XML.fragment(body_xml).children.map(&:to_s).join("\n")
   end
 
   def to_s
-    "#{timestamp} #{from_dispname}: #{body}"
+    "#{id} #{timestamp} #{from_dispname}: #{body}"
   end
   
 end
